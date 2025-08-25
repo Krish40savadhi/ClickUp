d309831c73c5abfe9702255c3a8591771c117aa0
Krish40savadhi <Krish@sarvadhi.com>
2025-08-25 16:10:35 +0530

    minor changes

--- ProjectManagementApp/src/services/api.js
+++ ProjectManagementApp/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL:import.meta.env.VITE_BACKEND_URL || " http://localhost:4000",
+    baseURL:import.meta.env.VITE_BACKEND_URL || "http://localhost:4000",
     headers:{"Content-Type":"application/json"}
 });  
